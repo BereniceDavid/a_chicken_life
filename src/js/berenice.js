ed8410bdde2345b8835883f279f8fdc0f16b275b
Bérénice David <berenice.david@hotmail.fr>
2016-12-15 12:09:11 +0100

    Update JSON

--- src/js/berenice.js
+++ src/js/berenice.js
@@ -7,9 +7,9 @@ console.log(fill_location_list);
 var upgrade = '';
 
 //fill var upgrade with the upgrade element we want to display
-function upgrade_html(src, name, cost, level) {
+function upgrade_html(src, name, cost, level, nbr_upgrade) {
 	upgrade = document.createElement("li");
-	upgrade.innerHTML = '<span class="logo_upgrade"><img src="'+ src +'" class="icon"></span><span class="name_upgrade">'+ name +'</span><img src="src/img/interface/gold.svg" class="gold_icon"><span class="price_upgrade">'+ cost +'</span><span class="unit_lvl">Lv'+ level +'</span><span class="number_upgrade">0</span>';
+	upgrade.innerHTML = '<span class="logo_upgrade"><img src="'+ src +'" class="icon"></span><span class="name_upgrade">'+ name +'</span><img src="src/img/interface/gold.svg" class="gold_icon"><span class="price_upgrade">'+ cost +'</span><span class="unit_lvl">Lv'+ level +'</span><span class="number_upgrade">'+ nbr_upgrade +'</span>';
 }
 
 
@@ -19,7 +19,7 @@ for(var k = 0; k < game.length; k++) {
 	if (!game[k].consumable_upgrades == "") {
 		
 		for(var l = 0; l < game[k].consumable_upgrades.length; l++) {
-			upgrade_html(game[k].consumable_upgrades[l].source, game[k].consumable_upgrades[l].name, game[k].consumable_upgrades[l].cost, game[k].level);
+			upgrade_html(game[k].consumable_upgrades[l].source, game[k].consumable_upgrades[l].name, game[k].consumable_upgrades[l].cost, game[k].level, '');
 			fill_location_list[0].appendChild(upgrade);
 		}
 	}
@@ -27,7 +27,7 @@ for(var k = 0; k < game.length; k++) {
 	//display habitat upgrades
 	if (!game[k].habitat_upgrades == "") {
 		for(var m = 0; m < game[k].habitat_upgrades.length; m++) {
-			upgrade_html(game[k].habitat_upgrades[m].source, game[k].habitat_upgrades[m].name, game[k].habitat_upgrades[m].cost, game[k].level);
+			upgrade_html(game[k].habitat_upgrades[m].source, game[k].habitat_upgrades[m].name, game[k].habitat_upgrades[m].cost, game[k].level, '0');
 			fill_location_list[2].appendChild(upgrade);
 		}
 	}
@@ -35,7 +35,7 @@ for(var k = 0; k < game.length; k++) {
 	//display habitats 
 	if (!game[k].habitat == "") {
 		for(var n = 0; n < game[k].habitat.length; n++) {
-			upgrade_html(game[k].habitat[n].source, game[k].habitat[n].name, game[k].habitat[n].cost, game[k].level);
+			upgrade_html(game[k].habitat[n].source, game[k].habitat[n].name, game[k].habitat[n].cost, game[k].level, '');
 			fill_location_list[1].appendChild(upgrade);
 		}
 	}
